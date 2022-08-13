57178203a9c1017bfccb5c2804396e4821fe3e8c
Source Academy User <No public email provided>
2022-08-13 23:45:36 +0800

    Changes made from Source Academy

---
+++ StudioS2.js
@@ -0,0 +1,30 @@
+function biggie_size(x) {
+    return x+4;
+}
+
+function unbiggie_size(x) {
+    return x-4;
+}
+
+function is_biggie_size(x) {
+    return x<=4
+    ? 1>2
+    : 1<2;
+}
+
+function combo_price(x) {
+    return x%2===0
+    ? 1.17*4 +(1.17+0.5)*math_floor(x/5)
+    :1.17*(x%4) + 0.5*math_floor(x/5);
+}
+
+function empty_order() {
+    return 0;
+}
+
+function add_to_order(x) {
+    
+
+}
+combo_price(5); 
+empty_order();
